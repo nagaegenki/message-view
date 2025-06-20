c0bf8945fd407a8a45ed435a04f1cbb1ae2a0f41
長江 <nagae-genki_ufig@outlook.com>
2025-06-21 00:22:27 +0900

    fix: bug fix to process repeating group appropriately

--- src/parser/fixParser.js
+++ src/parser/fixParser.js
@@ -36,32 +36,35 @@ export function parseFixMessage(message, delimiter, selectedDefs) {
 
       for (let g = 0; g < groupCount; g++) {
         const groupItem = [];
-        let j = 0;
+        const tmpGroupFields = [...groupFields];
 
-        while (j < groupFields.length) {
+        while (i + 1 < fields.length) {
           i++;
-
           const [nextTagRaw, ...nextValueParts] = fields[i].split("=");
           const nextTag = nextTagRaw;
           const nextValue = nextValueParts.join("=");
-          
-          const childDef = tagDefs[nextTag] || {};
-          groupItem.push({
-            tag: nextTag,
-            name: childDef.name || "Unknown",
-            description: childDef.description || "No description",
-            type: childDef.type || "",
-            value: nextValue,
-            enumLabel: childDef.enum?.[nextValue] || "",
-            repeating: true
-          });
 
-          if (groupFields.includes(nextTag)) {
-            j++;
+          // Check if the next tag is part of the group
+          if (tmpGroupFields.includes(nextTag)) {
+            tmpGroupFields.splice(tmpGroupFields.indexOf(nextTag), 1); // Remove tag from the list
+            const childDef = tagDefs[nextTag] || {};
+            groupItem.push({
+              tag: nextTag,
+              name: childDef.name || "Unknown",
+              description: childDef.description || "No description",
+              type: childDef.type || "",
+              value: nextValue,
+              enumLabel: childDef.enum?.[nextValue] || "",
+              repeating: true
+            });
+          } else {
+            i--;  // If the tag is not part of the group, we stop processing this group
+            break;
           }
         }
         children.push(groupItem);
       }
+      console.log("Group Fields:", children);
 
       result.push({
         tag,
